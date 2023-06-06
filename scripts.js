2389ecc9a954a87e0f21440ddb76d710ff05709c
Dark Cypher <134491076+Giddel-Wilson@users.noreply.github.com>
2023-06-06 23:34:21 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,5 @@
 function removeEmailInput() {
-    // Get the email input element
     var emailInput = document.querySelector('input[type="email"]');
-
-    // Remove the element from the DOM
     emailInput.remove();
 }
-
-// Add an event listener to the form submit button
 document.querySelector('button[type="submit"]').addEventListener('click', removeEmailInput);
