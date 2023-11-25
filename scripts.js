bff79eb7c201c68922f2508055df69173b961b53
Flash Tech <134491076+Giddel-Wilson@users.noreply.github.com>
2023-11-25 21:40:04 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-function removeEmailInput() {
-    var emailInput = document.querySelector('input[type="email"]');
-    emailInput.remove();
-}
-document.querySelector('button[type="submit"]').addEventListener('click', removeEmailInput);
